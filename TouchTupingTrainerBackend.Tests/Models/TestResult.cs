c41ba661e2817c2089b77d0211a7fa37da36d9d1
Denis1310 <sa.sz.uz096@gmail.com>
2025-02-14 16:25:38 +0200

    add: calculator tests

---
+++ TouchTupingTrainerBackend.Tests/Models/TestResult.cs
@@ -0,0 +1,10 @@
+﻿using TouchTypingTrainerBackend.Models;
+
+namespace TouchTupingTrainerBackend.Tests.Models
+{
+    public class TestResult : IUserResult
+    {
+        public float Accuracy { get; set; }
+        public int Speed { get; set; }
+    }
+}
---
+++ TouchTupingTrainerBackend.Tests/Services/CalcServiceTest.cs
@@ -0,0 +1,100 @@
+﻿using TouchTupingTrainerBackend.Tests.Models;
+using TouchTypingTrainerBackend.Services;
+using FluentAssertions;
+
+namespace TouchTupingTrainerBackend.Tests.Services
+{
+    public class CalcServiceTest
+    {
+        readonly private ICalcService _calcService;
+
+        public CalcServiceTest()
+        {
+            _calcService = new CalcService();
+        }
+
+        [Fact]
+        public void CalculatePerformance_ShouldReturnUserPerformanceResult()
+        {
+            // arrange
+            var resourse = "tnhaoeu hadeu niaoeu naehu eahodut naoetshu anehu";
+            var mistakesCount = 2;
+            var duration = 25;
+
+            var expectedResult = new TestResult
+            {
+                Speed = 117,
+                Accuracy = 95.92f
+            };
+
+            // act
+            TestResult result = _calcService.CalculatePerformance<TestResult>(resourse,
+                mistakesCount,
+                duration);
+
+            // assert
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Fact]
+        public void CalculatePerformance_WithoutMistakes_AccuracyResultShouldBeAThousand()
+        {
+            // arrange
+            var resourse = "tnhaoeu hadeu niaoeu naehu eahodut naoetshu anehu";
+            var mistakesCount = 0;
+            var duration = 25;
+
+            var expectedAccuracy = 100f;
+
+            // act
+            TestResult result = _calcService.CalculatePerformance<TestResult>(resourse,
+                mistakesCount,
+                duration);
+
+            // assert
+            Assert.Equal(expectedAccuracy, result.Accuracy);
+        }
+
+        [Fact]
+        public void CalculatePerformance_WhenMistakesCountIsMoreThanTheResourseLength_ShouldReturnZeroAccuracyPercent()
+        {
+            // arrange
+            var resourse = "tnhaoeu hadeu niaoeu naehu eahodut naoetshu anehu";
+            var mistakesCount = resourse.Length + 1;
+            var duration = 25;
+
+            var expectedAccuracy = 0f;
+
+            // act
+            TestResult result = _calcService.CalculatePerformance<TestResult>(resourse,
+                mistakesCount,
+                duration);
+
+            // assert
+            Assert.Equal(expectedAccuracy, result.Accuracy);
+        }
+
+        [Fact]
+        public void CalculatePerformance_WhenLengthIsEmptyOrDurationEqualZero_ShouldReturnEmptyResult()
+        {
+            // arrange
+            var resourse = "";
+            var mistakesCount = 2;
+            var duration = 0;
+
+            var expectedResult = new TestResult
+            {
+                Speed = 0,
+                Accuracy = 0f
+            };
+
+            // act
+            TestResult result = _calcService.CalculatePerformance<TestResult>(resourse,
+                mistakesCount,
+                duration);
+
+            // assert
+            result.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
