cf27c7ca42f7282e5c1bc3a52469fce250b07d74
Denis1310 <sa.sz.uz096@gmail.com>
2025-02-18 22:50:38 +0200

    add: entities tests

---
+++ TouchTupingTrainerBackend.Tests/Entities/LayoutKeyTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Moq;
+using System.Data.Common;
+using TouchTypingTrainerBackend.Entities;
+
+namespace TouchTupingTrainerBackend.Tests.Entities
+{
+    public class LayoutKeyTest
+    {
+        [Fact]
+        public void Map_ShouldReturnAMappedLayoutKey()
+        {
+            // arrange
+            var expectedIndex = 1;
+            var expectedValue = 'c';
+            var expectedShiftValue = 'C';
+
+            var value = "c";
+            var shiftValue = "C";
+
+            var drMock = new Mock<DbDataReader>();
+
+            drMock.Setup(r => r.GetOrdinal("KeyFID"))
+                .Returns(0);
+            drMock.Setup(r => r.GetInt32(0))
+                .Returns(expectedIndex);
+
+            drMock.Setup(r => r.GetOrdinal("Value"))
+                .Returns(1);
+            drMock.Setup(r => r.GetString(1))
+                .Returns(value);
+
+            drMock.Setup(r => r.GetOrdinal("ShiftValue"))
+                .Returns(2);
+            drMock.Setup(r => r.GetString(2))
+                .Returns(shiftValue);
+
+            // act
+            var result = LayoutKey.Map(drMock.Object);
+
+            // assert
+            Assert.Equal(expectedIndex, result.Index);
+            Assert.Equal(expectedValue, result.Value);
+            Assert.Equal(expectedShiftValue, result.ShiftValue);
+        }
+    }
+}
---
+++ TouchTupingTrainerBackend.Tests/Entities/LayoutTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Moq;
+using System.Data.Common;
+using TouchTypingTrainerBackend.Entities;
+
+namespace TouchTupingTrainerBackend.Tests.Entities
+{
+    public class LayoutTest
+    {
+        [Fact]
+        public void Map_ShouldReturnAMappedLayout()
+        {
+            // arrange
+            var expectedId = 1;
+            var expectedName = "qwerty";
+
+            var drMock = new Mock<DbDataReader>();
+
+            drMock.Setup(r => r.GetOrdinal("Layout_UID"))
+                .Returns(0);
+            drMock.Setup(r => r.GetInt32(0))
+                .Returns(expectedId);
+
+            drMock.Setup(r => r.GetOrdinal("Name"))
+                .Returns(1);
+            drMock.Setup(r => r.GetString(1))
+                .Returns(expectedName);
+
+            // act
+            var result = Layout.Map(drMock.Object);
+
+            // assert
+            Assert.Equal(expectedId, result.LayoutId);
+            Assert.Equal(expectedName, result.Name);
+        }
+    }
+}
